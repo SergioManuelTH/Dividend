6cfbe1278a6da1626877646187fd7a73e6d60017
sergio manuel <csth555@gmail.com>
2022-08-27 11:47:43 +0200

    prueba1

---
+++ contracts/Dividend.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract Dividend{
+
+string public name;
+address public owner;
+
+
+
+constructor (string memory _n){
+    name = _n;
+    owner = msg.sender;
+
+}
+
+    function incomeDividend (uint256 dividend) public payable returns (uint256 balance){
+        require (msg.sender == owner);
+        require (msg.value == dividend);
+        return address(this).balance;
+
+    }
+
+    function withdrawalDividend (uint256 amount) public {
+        require(address(this).balance >= amount);
+        payable(msg.sender).transfer(amount);
+        
+   }
+
+}
